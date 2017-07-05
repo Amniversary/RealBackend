f57f83dbecc92578b5692582d04dd14b6dfb456a
Gavean <623006297@qq.com>
2017-07-05 14:22:39 +0800

    update

--- backend/config/params.php
+++ backend/config/params.php
@@ -3,8 +3,5 @@ return [
     'adminEmail' => 'admin@example.com',
     'pwd_crypt_key'=>'uwjd7&#$!dhow1236547diyf',
 
-    'QiNiu'=>[
-        'ak'=>'Q_EXBiZJnxJZwaKVxQzD0IcakVA49oAONkmVsTH0',
-        'sk'=>'bzumZIZkLwgy4dLUwKwbgyzZAlFJy3E4TnXnE7Nh',
-    ],
+
 ];
